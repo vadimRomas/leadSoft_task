ab5d0a8ebd5eda10960f33de8ab5580507b21520
vadimRomas <romasvadim10@gmail.com>
2023-07-12 00:58:35 +0300

    CORS disabled

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -3,8 +3,9 @@ import {CustomCookies} from "../utils/cookies";
 export async function sentInfoAboutUser() {
     const endUserId = new CustomCookies().getEndUserId()
     const currentUrl = window.location.href
-    console.log(endUserId, currentUrl)
-    await fetch('http://0.0.0.0:5000/where', {
+
+    await fetch('http://localhost:5000/where', {
+        mode: "no-cors",
         method: 'POST',
         body: JSON.stringify({endUserId, webPageURL: currentUrl})
     })
